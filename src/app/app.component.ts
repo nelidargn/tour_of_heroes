d2a4974e6140f05023954468aae8a703e02a2979
nelida <nelidargn@gmail.com>
2025-10-29 13:06:39 -0600

    Display_a_selection_list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { HeroesComponent } from './heroes/heroes.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: false,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
