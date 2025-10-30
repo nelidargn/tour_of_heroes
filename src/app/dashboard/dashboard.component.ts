fff6906651502ed6452e805338301e85e638d18a
nelida <nelidargn@gmail.com>
2025-10-30 12:39:01 -0600

    Add_navigation_with_routing

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-dashboard',
   standalone: false,
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
-export class HeroesComponent implements OnInit {
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
@@ -20,6 +19,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,19 +1,36 @@
-import { Component, Input } from '@angular/core';
-import { CommonModule, NgIf, UpperCasePipe} from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {Hero} from '../hero';
-import { ComponentFixture } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: 'app-hero-detailCoponent',
-  standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, NgIf, UpperCasePipe],
+  selector: 'app-hero-detail',
+  standalone: false,
   templateUrl: './hero-detail.component.html',
-  styleUrl: './hero-detail.component.css',
+  styleUrls: [ './hero-detail.component.css' ]
 })
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
 
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
